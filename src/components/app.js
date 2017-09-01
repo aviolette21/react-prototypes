b351511fcad71caf1d9adc2e85aea061ea7cbd38
Austin Violette <violette.austin@yahoo.com>
2017-09-01 15:04:06 -0700

    react functional components - Austin Violette

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,26 @@
+import React from 'react';
+import Table from './table';
 
+const students = [{
+	student: 'Goku',
+	course: 'fighting',
+	grade: 100
+}, {
+	student: 'chichi',
+	course: 'nagging',
+	grade: 100
+}, {
+	student: 'gohan',
+	course: 'saiyan',
+	grade: 100
+}];
+
+export default () => {
+	return (
+		<div className="container">
+				<h1>Student Grade Table</h1>
+				<Table data={students}/>
+		</div>
+	)
+}
 
